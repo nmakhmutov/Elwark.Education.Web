e49a0227cea88f19cca824df15ddda9d305abfd0
Nail Makhmutov <makhmutov.nail@gmail.com>
2020-12-14 06:23:28 +0300

    Added json serialization settings

---
+++ Elwark.Education.Web/Infrastructure/Json.cs
@@ -0,0 +1,23 @@
+using Elwark.Education.Web.Infrastructure.Converters;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+
+namespace Elwark.Education.Web.Infrastructure
+{
+    internal static class Json
+    {
+        public static readonly JsonSerializer Serializer = new()
+        {
+            Formatting = Formatting.None,
+            NullValueHandling = NullValueHandling.Ignore,
+            ContractResolver = new CamelCasePropertyNamesContractResolver(),
+            Converters =
+            {
+                new IsoDateTimeConverter(),
+                new StringEnumConverter(new CamelCaseNamingStrategy()),
+                new TestConclusionJsonConverter()
+            }
+        };
+    }
+}
