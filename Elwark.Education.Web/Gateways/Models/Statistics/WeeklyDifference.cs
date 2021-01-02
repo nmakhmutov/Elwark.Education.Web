c38efe41fb53c702d03738e9aaea4999a08534f0
Nail Makhmutov <makhmutov.nail@gmail.com>
2021-01-02 15:51:03 +0300

    Localization fix

--- Elwark.Education.Web/Gateways/Models/Statistics/WeeklyDifference.cs
+++ Elwark.Education.Web/Gateways/Models/Statistics/WeeklyDifference.cs
@@ -5,7 +5,7 @@ namespace Elwark.Education.Web.Gateways.Models.Statistics
     public sealed record WeeklyDifferenceItem<T>(T Current, T Difference);
 
     public sealed record WeeklyDifference(
-        WeeklyDifferenceItem<int> PassedTests,
+        WeeklyDifferenceItem<int> CompletedTests,
         WeeklyDifferenceItem<long> Score,
         WeeklyDifferenceItem<int> Questions,
         WeeklyDifferenceItem<TimeSpan> TimeSpent
