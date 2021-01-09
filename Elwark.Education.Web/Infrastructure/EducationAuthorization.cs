4d29485e139d2a3b354498131b166f9e7e35961b
Nail Makhmutov <makhmutov.nail@gmail.com>
2021-01-09 14:06:03 +0300

    Authorization options

--- Elwark.Education.Web/Infrastructure/EducationAuthorization.cs
+++ Elwark.Education.Web/Infrastructure/EducationAuthorization.cs
@@ -1,15 +1,14 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.Extensions.Options;
 
 namespace Elwark.Education.Web.Infrastructure
 {
     public class EducationAuthorization : AuthorizationMessageHandler
     {
-        public EducationAuthorization(IAccessTokenProvider provider, NavigationManager navigation, UrlsOptions options) 
+        public EducationAuthorization(IAccessTokenProvider provider, NavigationManager navigation, UrlsOptions urls,
+            IOptions<RemoteAuthenticationOptions<OidcProviderOptions>> oidc)
             : base(provider, navigation) =>
-            ConfigureHandler(
-                new[] {options.Gateway.ToString()},
-                new[] {"elwark.education.web"}
-            );
+            ConfigureHandler(new[] {urls.Gateway.ToString()}, oidc.Value.ProviderOptions.DefaultScopes);
     }
 }
