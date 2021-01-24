14621e5b68916aea04ae819443bcabd4a2a411de
Nail Makhmutov <makhmutov.nail@gmail.com>
2021-01-24 07:58:53 +0300

    New profile page

--- Elwark.Education.Web/Pages/Links.cs
+++ Elwark.Education.Web/Pages/Links.cs
@@ -1,3 +1,4 @@
+using System;
 using Elwark.Education.Web.Gateways.Models.History;
 using Elwark.Education.Web.Model;
 
@@ -11,14 +12,14 @@ namespace Elwark.Education.Web.Pages
         {
             public const string Index = "/profile";
 
-            public static string Statistics(SubjectType subjectType) =>
-                $"{Index}/{subjectType.ToString().ToLowerInvariant()}";
+            public static string Statistics(SubjectType type) =>
+                $"{Index}/{type.ToString().ToLowerInvariant()}";
 
-            public static string Tests(SubjectType subjectType) =>
-                $"{Index}/{subjectType.ToString().ToLowerInvariant()}/tests";
+            public static string Tests(SubjectType type) =>
+                $"{Index}/{type.ToString().ToLowerInvariant()}/tests";
 
-            public static string TestDetail(SubjectType subjectType, string testId) =>
-                $"{Tests(subjectType)}/{testId}";
+            public static string TestDetail(SubjectType type, string testId) =>
+                $"{Tests(type)}/{testId}";
         }
 
         public static class History
@@ -47,6 +48,10 @@ namespace Elwark.Education.Web.Pages
 
             public static string Article(string articleId) =>
                 $"{Index}/article/{articleId}";
+            
+            public static string Test(string testId) =>
+                $"{Index}/test/{testId}";
+            
         }
 
         public static class Astronomy
@@ -58,12 +63,15 @@ namespace Elwark.Education.Web.Pages
 
             public static string Article(string articleId) =>
                 $"{Index}/article/{articleId}";
+            
+            public static string Test(string testId) =>
+                $"{Index}/test/{testId}";
         }
 
         public static class Subjects
         {
-            public static string Index(SubjectType subjectType) =>
-                subjectType switch
+            public static string Index(SubjectType type) =>
+                type switch
                 {
                     SubjectType.History => History.Index,
                     SubjectType.Physics => Physics.Index,
@@ -71,8 +79,8 @@ namespace Elwark.Education.Web.Pages
                     _ => string.Empty
                 };
 
-            public static string Topic(SubjectType subjectType, string topicId) =>
-                subjectType switch
+            public static string Topic(SubjectType type, string topicId) =>
+                type switch
                 {
                     SubjectType.History => History.Topic(topicId),
                     SubjectType.Physics => Physics.Topic(topicId),
@@ -80,12 +88,21 @@ namespace Elwark.Education.Web.Pages
                     _ => string.Empty
                 };
 
-            public static string Article(SubjectType subjectType, string articleId) =>
-                subjectType switch
+            public static string Article(SubjectType type, string articleId) =>
+                type switch
                 {
                     SubjectType.History => History.Article(articleId),
                     SubjectType.Physics => Physics.Article(articleId),
                     SubjectType.Astronomy => Astronomy.Article(articleId),
+                    _ => string.Empty
+                };
+
+            public static string Test(SubjectType type, string testId) =>
+                type switch
+                {
+                    SubjectType.History => History.Test(testId),
+                    SubjectType.Physics => Physics.Test(testId),
+                    SubjectType.Astronomy => Astronomy.Test(testId),
                     _ => string.Empty
                 };
         }
