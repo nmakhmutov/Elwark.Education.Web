793815284046f9c9b194a6cb349dd7cf9793c015
Nail Makhmutov <makhmutov.nail@gmail.com>
2021-12-31 18:51:39 +0300

    Activity statistics page

--- src/Education.Client/Gateways/History/Me/ActivityStatistics.cs
+++ src/Education.Client/Gateways/History/Me/ActivityStatistics.cs
@@ -1,3 +1,23 @@
+using System;
+using Education.Client.Gateways.Models.Progress;
+
 namespace Education.Client.Gateways.History.Me;
 
-public sealed record ActivityStatistics(Activity[] Daily, Activity[] Monthly);
+public sealed record ActivityStatistics(
+    Contrast<ulong> EasyTests,
+    Contrast<ulong> HardTests,
+    Contrast<ulong> MixedTests,
+    Contrast<uint> EventGuessers,
+    ActivityStatistics.Statistics[] Daily,
+    ActivityStatistics.Statistics[] Monthly
+)
+{
+    public sealed record Statistics(
+        DateTime Date,
+        uint Total,
+        uint EasyTests,
+        uint HardTests,
+        uint MixedTests,
+        uint EventGuessers
+    );
+}
--- src/Education.Client/Gateways/Models/Test/QuestionType.cs
+++ src/Education.Client/Gateways/Models/Test/QuestionType.cs
@@ -5,5 +5,5 @@ public enum QuestionType
     TextAnswer = 0,
     OneAnswer = 1,
     ManyAnswers = 2,
-    SortedAnswers = 3,
+    SortedAnswers = 3
 }
