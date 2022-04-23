e76148c3f292fc51a75690045d6170a5a468947a
Nail Makhmutov <makhmutov.nail@gmail.com>
2022-04-23 09:15:53 +0300

    Changed global ranking table

--- src/Pages/CommonLinks.cs
+++ src/Pages/CommonLinks.cs
@@ -5,7 +5,7 @@ public static class CommonLinks
     public const string Root = "/";
 
     public const string Account = "/account";
-    
+
     public const string LogOut = "/authentication/logout";
 
     public static string LogIn(string returnUrl) =>
