1976eb46cfcc3bececbd755b5cc212e181bee8e7
Nail Makhmutov <makhmutov.nail@gmail.com>
2023-02-16 09:15:46 +0300

    Added filtering

--- src/Education.Web.Client/Features/History/Services/Store/IHistoryStoreService.cs
+++ src/Education.Web.Client/Features/History/Services/Store/IHistoryStoreService.cs
@@ -1,4 +1,5 @@
 using Education.Web.Client.Features.History.Services.Store.Model;
+using Education.Web.Client.Features.History.Services.Store.Request;
 using Education.Web.Client.Http;
 
 namespace Education.Web.Client.Features.History.Services.Store;
---
+++ src/Education.Web.Client/Features/History/Services/Store/Request/CheckoutRequest.cs
@@ -0,0 +1,3 @@
+namespace Education.Web.Client.Features.History.Services.Store.Request;
+
+public sealed record CheckoutRequest(uint ProductId, byte Quantity);
