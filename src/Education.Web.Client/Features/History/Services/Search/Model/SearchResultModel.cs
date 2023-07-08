4b87d5a257cd5b5f7619f4fe6d06ac9208d2d007
Nail Makhmutov <makhmutov.nail@gmail.com>
2023-07-08 09:27:05 +0400

    Added search bar for search page

--- src/Education.Web.Client/Features/History/Services/Search/Model/SearchResultModel.cs
+++ src/Education.Web.Client/Features/History/Services/Search/Model/SearchResultModel.cs
@@ -1,3 +1,6 @@
 namespace Education.Web.Client.Features.History.Services.Search.Model;
 
-public sealed record SearchResultModel(SearchModel[] Hints, IReadOnlyDictionary<string, int>? Categories, long Count);
+public sealed record SearchResultModel(SearchModel[] Hints, IReadOnlyDictionary<string, int>? Categories, long Count)
+{
+    public static SearchResultModel Empty = new(Array.Empty<SearchModel>(), new Dictionary<string, int>(), 0);
+}
--- src/Education.Web.Client/Features/History/Services/Search/Request/SearchRequest.cs
+++ src/Education.Web.Client/Features/History/Services/Search/Request/SearchRequest.cs
@@ -1,6 +1,6 @@
 namespace Education.Web.Client.Features.History.Services.Search.Request;
 
-public sealed record SearchRequest(string Q, bool IncludeCategories, string[] OnlyCategories, int Offset, int Limit)
+public sealed record SearchRequest(string Q, bool IncludeCategories, string[] Categories, int Offset, int Limit)
 {
     public SearchRequest(string q, int offset, int limit)
         : this(q, false, Array.Empty<string>(), offset, limit)
