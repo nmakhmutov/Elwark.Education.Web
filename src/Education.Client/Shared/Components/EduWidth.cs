617fbdfd5f591ee5d9e69a8e4876456da8ead449
Neil <makhmutov.nail@gmail.com>
2024-03-17 12:16:13 +0400

    Changed titles for main page cards

--- src/Education.Client/Shared/Components/EduWidth.cs
+++ src/Education.Client/Shared/Components/EduWidth.cs
@@ -2,12 +2,12 @@ namespace Education.Client.Shared.Components;
 
 public enum EduWidth
 {
-    None,
-    W600,
-    W960,
-    W1280,
-    W1440,
-    W1600,
-    W1920,
-    W2560
+    None = 0,
+    W600 = 600,
+    W960 = 960,
+    W1280 = 1280,
+    W1440 = 1440,
+    W1600 = 1600,
+    W1920 = 1920,
+    W2560 = 2560
 }
