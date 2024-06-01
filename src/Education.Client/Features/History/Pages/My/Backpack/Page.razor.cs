7c8c5277f92196b340450651e939265e9f548d85
Neil <makhmutov.nail@gmail.com>
2024-06-01 08:58:43 +0400

    Disableable footer in history layout

--- src/Education.Client/Features/History/Pages/My/Backpack/Page.razor.cs
+++ src/Education.Client/Features/History/Pages/My/Backpack/Page.razor.cs
@@ -3,6 +3,7 @@ using Education.Client.Features.History.Clients.Product;
 using Education.Client.Features.History.Clients.Product.Model;
 using Education.Client.Features.History.Clients.User;
 using Education.Client.Features.History.Clients.User.Model;
+using Education.Client.Features.History.Layout;
 using Education.Client.Models.Inventory;
 using Education.Client.Shared.Customer;
 using Microsoft.AspNetCore.Components;
@@ -10,9 +11,10 @@ using Microsoft.Extensions.Localization;
 
 namespace Education.Client.Features.History.Pages.My.Backpack;
 
-public sealed partial class Page : ComponentBase
+public sealed partial class Page : ComponentBase,
+    IDisposable
 {
-    private string _containerClass = "";
+    private string? _containerClass;
     private BackpackInventoryModel? _selectedInventory;
     private ProfileModel _profile = ProfileModel.Empty;
     private ApiResult<BackpackModel> _backpack = ApiResult<BackpackModel>.Loading();
@@ -30,11 +32,15 @@ public sealed partial class Page : ComponentBase
     [CascadingParameter]
     public CustomerState Customer { get; init; } = default!;
 
+    [CascadingParameter]
+    public HistoryLayout Layout { get; set; } = default!;
+
     protected override async Task OnInitializedAsync()
     {
+        Layout.HideFooter();
+
         var response = await UserClient.GetProfileAsync();
-        _profile = response
-            .Map(x => x)
+        _profile = response.Map(x => x)
             .UnwrapOrElse(() => _profile);
 
         _backpack = await UserClient.GetBackpackAsync();
@@ -52,9 +58,12 @@ public sealed partial class Page : ComponentBase
     private async Task OnInventorySelected(BackpackInventoryModel inventory)
     {
         _containerClass = "details-mode";
-        _product = ApiResult<ProductOverviewModel>.Loading();
-
         _selectedInventory = inventory;
+
+        _product = ApiResult<ProductOverviewModel>.Loading();
         _product = await ProductClient.GetAsync(inventory.ProductId);
     }
+
+    public void Dispose() =>
+        Layout.ShowFooter();
 }
